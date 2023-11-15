d0d8f3c3e12215e8b5d4c8e00b5730fd47a1eb98
Pinkk-Flag <antgera7@gmail.com>
2023-11-15 12:28:00 +1100

    Initial Commit

---
+++ Classic Snake!/Form1.Designer.vb
@@ -0,0 +1,163 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmSnake
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmSnake))
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.StartToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EndToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblScore = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.pnlGameArea = New System.Windows.Forms.Panel()
+        Me.picFood = New System.Windows.Forms.PictureBox()
+        Me.picSnakePiece = New System.Windows.Forms.PictureBox()
+        Me.tmrGame = New System.Windows.Forms.Timer(Me.components)
+        Me.MenuStrip1.SuspendLayout()
+        Me.pnlGameArea.SuspendLayout()
+        CType(Me.picFood, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picSnakePiece, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(24, 24)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.StartToolStripMenuItem, Me.EndToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(800, 33)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'StartToolStripMenuItem
+        '
+        Me.StartToolStripMenuItem.Name = "StartToolStripMenuItem"
+        Me.StartToolStripMenuItem.Size = New System.Drawing.Size(64, 29)
+        Me.StartToolStripMenuItem.Text = "Start"
+        '
+        'EndToolStripMenuItem
+        '
+        Me.EndToolStripMenuItem.Name = "EndToolStripMenuItem"
+        Me.EndToolStripMenuItem.Size = New System.Drawing.Size(58, 29)
+        Me.EndToolStripMenuItem.Text = "End"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Times New Roman", 16.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(658, 124)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(101, 36)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Score!"
+        '
+        'lblScore
+        '
+        Me.lblScore.AutoSize = True
+        Me.lblScore.Font = New System.Drawing.Font("Times New Roman", 28.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.lblScore.Location = New System.Drawing.Point(681, 173)
+        Me.lblScore.Name = "lblScore"
+        Me.lblScore.Size = New System.Drawing.Size(55, 63)
+        Me.lblScore.TabIndex = 2
+        Me.lblScore.Text = "0"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Times New Roman", 28.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label3.Location = New System.Drawing.Point(208, 33)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(375, 63)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Classic Snake!"
+        '
+        'pnlGameArea
+        '
+        Me.pnlGameArea.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.pnlGameArea.Controls.Add(Me.picFood)
+        Me.pnlGameArea.Location = New System.Drawing.Point(12, 118)
+        Me.pnlGameArea.Name = "pnlGameArea"
+        Me.pnlGameArea.Size = New System.Drawing.Size(631, 320)
+        Me.pnlGameArea.TabIndex = 4
+        '
+        'picFood
+        '
+        Me.picFood.Image = CType(resources.GetObject("picFood.Image"), System.Drawing.Image)
+        Me.picFood.Location = New System.Drawing.Point(70, 82)
+        Me.picFood.Name = "picFood"
+        Me.picFood.Size = New System.Drawing.Size(34, 36)
+        Me.picFood.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picFood.TabIndex = 1
+        Me.picFood.TabStop = False
+        '
+        'picSnakePiece
+        '
+        Me.picSnakePiece.Image = Global.Classic_Snake_.My.Resources.Resources.BALL
+        Me.picSnakePiece.Location = New System.Drawing.Point(658, 319)
+        Me.picSnakePiece.Name = "picSnakePiece"
+        Me.picSnakePiece.Size = New System.Drawing.Size(35, 36)
+        Me.picSnakePiece.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picSnakePiece.TabIndex = 0
+        Me.picSnakePiece.TabStop = False
+        '
+        'tmrGame
+        '
+        Me.tmrGame.Interval = 200
+        '
+        'frmSnake
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.pnlGameArea)
+        Me.Controls.Add(Me.picSnakePiece)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.lblScore)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "frmSnake"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.pnlGameArea.ResumeLayout(False)
+        CType(Me.picFood, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picSnakePiece, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents StartToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EndToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblScore As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents pnlGameArea As Panel
+    Friend WithEvents picSnakePiece As PictureBox
+    Friend WithEvents picFood As PictureBox
+    Friend WithEvents tmrGame As Timer
+End Class
---
+++ Classic Snake!/Form1.vb
@@ -0,0 +1,148 @@
+﻿'=======================================================================
+' Snake Game - frmSnake Class
+' Made by Dario Gerald
+' 13/11/23 9:19am
+' GitHub: https://github.com/Pinkk-Flag
+'=======================================================================
+
+Public Class frmSnake
+
+    ' Variables for movement and game state
+    Dim deltaX As Integer
+    Dim deltaY As Integer
+    Dim SnakeLoop = 0
+    Dim SnakeLength As Integer
+    Dim LastX As Integer = 0
+    Dim Lasty As Integer = 0
+    Dim finished As Boolean
+    Dim picSnake(99) As PictureBox
+
+    ' Handles arrow key input for snake movement
+    Private Sub frmSnake_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        ' Adjusts the movement direction based on the pressed key
+        If e.KeyCode = Keys.Right Then
+            deltaX = 35
+            deltaY = 0
+        ElseIf e.KeyCode = Keys.Left Then
+            deltaX = -35
+            deltaY = 0
+        ElseIf e.KeyCode = Keys.Up Then
+            deltaY = -35
+            deltaX = 0
+        ElseIf e.KeyCode = Keys.Down Then
+            deltaY = 35
+            deltaX = 0
+        End If
+    End Sub
+
+    ' Updates the game state on each timer tick
+    Private Sub tmrGame_Tick(sender As Object, e As EventArgs) Handles tmrGame.Tick
+        ' Stores the position of the last snake piece
+        LastX = picSnake(SnakeLength - 1).Left
+        Lasty = picSnake(SnakeLength - 1).Top
+
+        ' Moves each snake piece to the position of the preceding piece
+        If SnakeLength > 1 Then
+            For SnakeLoop = SnakeLength - 1 To 1 Step -1
+                picSnake(SnakeLoop).Left = picSnake(SnakeLoop - 1).Left
+                picSnake(SnakeLoop).Top = picSnake(SnakeLoop - 1).Top
+            Next SnakeLoop
+        End If
+
+        ' Moves the head of the snake based on the current direction
+        picSnake(0).Left = picSnake(0).Left + deltaX
+        picSnake(0).Top = picSnake(0).Top + deltaY
+
+        ' Handles collision with food and adjusts snake length
+        If Collide(picSnake(0), picFood) Then
+            lblScore.Text = Val(lblScore.Text) + 1
+            ' Randomly places new food within the game area
+            picFood.Top = Rnd() * (pnlGameArea.Height - picFood.Height)
+            picFood.Left = Rnd() * (pnlGameArea.Width - picFood.Width)
+            ' Increases snake length and makes the new piece visible
+            If SnakeLength <= 100 Then
+                SnakeLength = SnakeLength + 1
+                picSnake(SnakeLength - 1).Visible = True
+                picSnake(SnakeLength - 1).Left = LastX
+                picSnake(SnakeLength - 1).Top = Lasty
+            End If
+        End If
+
+        ' Checks for collisions with the snake's body or game boundaries
+        For SnakeLoop = SnakeLength - 1 To 1 Step -1
+            If Collide(picSnake(SnakeLoop), picSnake(0)) Then
+                finished = True
+            End If
+        Next
+        If picSnake(0).Left < 0 Or picSnake(0).Left + picSnake(0).Width > pnlGameArea.Width Or
+            picSnake(0).Top < 0 Or picSnake(0).Top + picSnake(0).Height > pnlGameArea.Height Then
+            finished = True
+        End If
+
+        ' Ends the game if a collision occurs
+        If finished Then
+            tmrGame.Enabled = False
+            MsgBox("Game over")
+        End If
+    End Sub
+
+    ' Initializes the snake on form validation change
+    Private Sub frmSnake_CausesValidationChanged(sender As Object, e As EventArgs) Handles Me.CausesValidationChanged
+        If picSnakePiece IsNot Nothing Then ' Add this check
+            InitializeSnake()
+        End If
+    End Sub
+
+    ' Checks for collision between two objects
+    Private Function Collide(ByVal ObjA As Object, ByVal ObjB As Object) As Boolean
+        If ObjA.Bounds.IntersectsWith(ObjB.Bounds) Then
+            Collide = True
+        Else
+            Collide = False
+        End If
+    End Function
+
+    ' Exits the application when the "End" menu item is clicked
+    Private Sub EndToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles EndToolStripMenuItem.Click
+        End
+    End Sub
+
+    ' Starts the game when the "Start" menu item is clicked
+    Private Sub StartToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles StartToolStripMenuItem.Click
+        tmrGame.Enabled = True
+        lblScore.Text = "0"
+        finished = False
+        picSnake(0).Left = 200
+        picSnake(0).Top = 200
+        picSnake(0).Visible = True
+        For SnakeLoop = 1 To 99
+            picSnake(SnakeLoop).Visible = False
+        Next SnakeLoop
+        SnakeLength = 1
+        deltaX = 35
+        deltaY = 0
+    End Sub
+
+    ' Initializes the snake on form load
+    Private Sub frmSnake_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        InitializeSnake()
+    End Sub
+
+    ' Initializes the snake array of picture boxes
+    Private Sub InitializeSnake()
+        For SnakeLoop = 0 To 99
+            picSnake(SnakeLoop) = New PictureBox()
+            picSnake(SnakeLoop).SizeMode = PictureBoxSizeMode.AutoSize
+            picSnake(SnakeLoop).Left = 200
+            picSnake(SnakeLoop).Top = 200
+            picSnake(SnakeLoop).Image = picSnakePiece.Image
+            If SnakeLoop = 0 Then
+                picSnake(SnakeLoop).Visible = True
+            Else
+                picSnake(SnakeLoop).Visible = False
+            End If
+            pnlGameArea.Controls.Add(picSnake(SnakeLoop))
+        Next SnakeLoop
+    End Sub
+
+End Class
